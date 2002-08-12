9092df100dc35728a759e2305256e1ebd27f2a44
alastair <alastair>
2002-08-12 18:13:33 +0000

    Tidy up preferences dialogue box, adding context sensitive help

--- Run/prefs.tcl
+++ Run/prefs.tcl
@@ -110,7 +110,8 @@ proc PrefValueSet { varName value } {
 # A user interface to the preference items.
 #
 proc Pref_HelpCommand { Page } {
-    tk_messageBox -message "$Page help to be implemented " -type ok
+# A single help page covers all the notebook pages at present, so the Page variable is ignored.
+     ContextSensitiveHelp .pref  diagrams/preferences.htm
 }
 
 proc Pref_Apply {} {
@@ -125,31 +126,47 @@ proc Pref_Dialog {} {
     } else  {
         set dlg [toplevel .pref]
         wm title .pref "Preferences"
+        wm resizable .pref 0 0 
         ButtonBox $dlg.bbox -default 0
-        $dlg.bbox add -name ok -text OK -underline 0 -command {PrefSave}
-        $dlg.bbox add -name cancel -text Cancel -underline 0  -command {PrefDismiss}
-        $dlg.bbox add -name default -text Default -underline 0 \
+        $dlg.bbox add -name ok -text OK -underline 0 -width 8  \
+                -command {PrefSave}
+        $dlg.bbox add -name cancel -text Cancel -underline 0 -width 8 \
+                -command {PrefDismiss}
+        $dlg.bbox add -name default -text Default -underline 0 -width 8 \
                 -command {PrefReset ; PrefDismiss}
-        $dlg.bbox add -name help -text Help -underline 0 -command "::Pref_HelpCommand Preferences"
-        pack $dlg.bbox -side bottom -anchor e
-        
-        set notebook [NoteBook $dlg.notebook -height 350 -width 550]
+        $dlg.bbox add -name help -text Help -underline 0 -width 8 \
+                -command "::Pref_HelpCommand Preferences"
+#        pack $dlg.bbox -side bottom -anchor e -padx 4 -pady 4
+        pack $dlg.bbox -side bottom -padx 4 -pady 8       
+#        set notebook [NoteBook $dlg.notebook -height 220 -width 440]
+        set notebook [NoteBook $dlg.notebook ]
         $notebook insert end View -text View
-            set displayTF [TitleFrame [$notebook getframe View].displayTF \
-                -text "In new windows, display:"]
+            set displayTF [TitleFrame [$notebook getframe View].displayTF -text "In new windows, display:"]
             set displayF [$displayTF getframe]
-            set popupTF [TitleFrame [$notebook getframe View].popuptTF \
-                -text "Display popups over model components for:"]
+            set barTF [TitleFrame [$notebook getframe View].barTF -text "Tool bars:"]
+            set barF [$barTF getframe]
+            set popupTF [TitleFrame [$notebook getframe View].popuptTF -text "Popups over model components:"]
             set popupF [$popupTF getframe]
-            set miscViewF [frame [$notebook getframe View].miscViewF -relief groove ]
         $notebook insert end Edit -text Edit
+            set linkTF [TitleFrame [$notebook getframe Edit].linkTF -text "Links:"]
+            set linkF [$linkTF getframe]
+            set flowTF [TitleFrame [$notebook getframe Edit].flowTF -text "Flows:"]
+            set flowF [$flowTF getframe]
         $notebook insert end Build -text Build
-            set manyWinTF [TitleFrame [$notebook getframe Build].manyWintf \
-                    -text "If using multiple window run time environment, position of:"]
+            set oneWinTF [TitleFrame [$notebook getframe Build].oneWinTF -text "Run time environment:"]
+            set oneWinF [$oneWinTF getframe]
+            set manyWinTF [TitleFrame [$notebook getframe Build].manyWinTF -text "Window positions:"]
             set manyWinF [$manyWinTF getframe]
-            $notebook insert end Save -text Save
+            set compTF [TitleFrame [$notebook getframe Build].compTF -text "C++ compiler:"]
+            set compF [$compTF getframe]
+        $notebook insert end Save -text Save
+            set canvasTF [TitleFrame [$notebook getframe Save].canvasTF -text "Save optimised canvas data:"]
+            set canvasF [$canvasTF getframe]
+            set recentTF [TitleFrame [$notebook getframe Save].recentTF -text "Recently used files:"]
+            set recentF [$recentTF getframe]
         $notebook raise View
-        pack $displayTF $popupTF $miscViewF $manyWinTF $notebook  -fill both
+        pack $displayTF $popupTF $barTF $linkTF $flowTF $oneWinTF $manyWinTF $compTF \
+                $canvasTF $recentTF $notebook -fill x -padx 4 -pady 4
         
 #        ShowMessage debug info "$pref(items)" ok
         
@@ -161,24 +178,25 @@ proc Pref_Dialog {} {
             }
        }
        set pref(uid) 0
-        foreach item $pref(items) {
+       foreach item $pref(items) {
 #            ShowMessage debug info "$item; [PrefRes $item]" ok
             switch -glob -- [PrefRes $item] {
                 init* {set frame $displayF}
-                compChoice {set frame [$notebook getframe Build]}
+                compChoice {set frame $compF}
                 comp* {set frame $popupF}
-                bigButtons {set frame $miscViewF}
-                flowRouting {set frame [$notebook getframe Edit]}
-                deleteEndToEnd {set frame [$notebook getframe Edit]}
-                saveExtras {set frame [$notebook getframe Save]}
-                recentCount {set frame [$notebook getframe Save]}
+                bigButtons {set frame $barF}
+                deleteEndToEnd {set frame $linkF}
+                flowRouting {set frame $flowF}
+                saveExtras {set frame $canvasF}
+                recentCount {set frame $recentF}
+                helperManager {set frame $oneWinF}
                 runControlPosition {set frame $manyWinF}
                 slidersPosition {set frame $manyWinF}
-                helperManager {set frame [$notebook getframe Build]}
                 default {set frame $displayTF}
             }
             PrefDialogItem $frame $item $maxWidth
-       }       
+       }
+       $notebook compute_size     
     }
 }
 
@@ -191,32 +209,37 @@ proc PrefDialogItem { frame item width } {
     global pref
     incr pref(uid)
     set f [frame $frame.p$pref(uid) -borderwidth 2]
-    pack $f -fill x
-    label $f.label -text [PrefComment $item] -width $width
-    bind $f.label <Enter> [list QueuePopup \
-            "AddWidgetPopup [PrefRes $item] %X %Y"]
+    pack $f -fill x -anchor w -side left -expand on
+# No longer use consistent width -- each label is allowed its own
+#    label $f.label -text [PrefComment $item] -width $width
+    label $f.label -text [PrefComment $item] -anchor w
+    bind $f.label <Enter> [list QueuePopup "AddWidgetPopup [PrefRes $item] %X %Y"]
     bind $f.label <Leave> RemovePopup
-    pack $f.label -side left
+# Delay packing label until we know whether it goes to the left or right of the item
+#    pack $f.label -side left -anchor w
     set default [PrefDefault $item]
     if {[regexp "^CHOICE " $default]} {
         foreach choice [lreplace $default 0 0] {
             incr pref(uid)
             radiobutton $f.c$pref(uid) -text $choice \
                     -variable [PrefVar $item] -value $choice
-            pack $f.c$pref(uid) -side left
+            pack $f.label -side left -anchor w -padx 2 -pady 2
+            pack $f.c$pref(uid) -side left -padx 2 -pady 2
         }
     } else {
         if {$default == "OFF" || $default == "ON"} {
             # This is a boolean
             set varName [PrefVar $item]
-            checkbutton $f.check -variable $varName \
-                    -command [list PrefFixupBoolean $f.check $varName]
-            PrefFixupBoolean $f.check $varName
-            pack $f.check -side left
+# Don't display on or off labels next to checkboxes
+#            checkbutton $f.check -variable $varName -command [list PrefFixupBoolean $f.check $varName]
+            checkbutton $f.check -variable $varName 
+            pack $f.check -side left -padx 2 -pady 2
+            pack $f.label -side left -anchor w -fill x -expand on -padx 2 -pady 2
         } else {
             # This is a string or numeric
             entry $f.entry -width 10 -relief sunken
-			pack $f.entry -side left -fill x -expand true
+            pack $f.label -side left -anchor w -padx 2 -pady 2
+            pack $f.entry -side left -fill x -padx 2 -pady 2
 			set pref(entry,[PrefVar $item]) $f.entry
 			set varName [PrefVar $item]
 			$f.entry insert 0 [uplevel #0 [list set $varName]]
@@ -225,13 +248,14 @@ proc PrefDialogItem { frame item width } {
 	}
 }
 proc PrefFixupBoolean {check varname} {
-	upvar #0 $varname var
+    upvar #0 $varname var
+    # This routine is not called
 	# Update the checkbutton text each time it changes
 	if {$var} {
-		$check config -text On
-	} else {
-		$check config -text Off
-	}
+        $check config -text "On"
+    } else {
+        $check config -text "Off"
+    }
 }
 proc PrefEntrySet { entry varName } {
 	PrefValueSet $varName [$entry get]
@@ -273,8 +297,8 @@ proc PrefSave {} {
 			puts $out $line
 		}
 	}
-	puts $out "!!! Lines below here automatically added"
-	puts $out "!!! <put date here>"
+    puts $out "!!! Lines below here automatically added"
+    puts $out [clock format [clock seconds] -format "!!! %a %d %b %Y, %H:%M"]
 	puts $out "!!! Do not edit below here"
 # next line is the anti-dibble -- a bug in the example program to check
 # if students have been paying attention
@@ -309,10 +333,11 @@ proc PrefSave {} {
 
 proc PrefReset {} {
 	global pref
-	# Re-read user defaults
+    # Re-read user defaults
+    # This isn't quite right...
 	option clear
 	PrefReadFile $pref(appDefaults) startup
-	PrefReadFile $pref(userDefaults) user
+#	PrefReadFile $pref(userDefaults) user
 	# Clear variables
 	set items $pref(items)
 	set pref(items) {}
