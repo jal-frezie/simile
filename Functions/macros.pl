f6e12dde2fd5f699df5af5d854910260f3a57b85
jmm <jmm>
2003-02-20 11:52:43 +0000

    Removed npv that didn't work properly. Added sgn and howmanytrue

--- Functions/macros.pl
+++ Functions/macros.pl
@@ -1,12 +1,15 @@
 subtotals(Arr) --> [st]=makearray((if place_in(1)==1 then 0.0 else
-	element(sofar([st]),place_in(1)-1))+element(Arr,place_in(1)),
-	count(Arr)),[st].
+element(sofar([st]),place_in(1)-1))+element(Arr,place_in(1)),
+count(Arr)),[st].
 rankings(L) --> sum(makearray(if element(L,place_in(1))>=L then 1 else 0,
-				count(L))).
+count(L))).
 colin(List) --> legg=rand_var(0,sum(List)),
-		1+sum(if subtotals(List)<legg then 1 else 0).
+1+sum(if subtotals(List)<legg then 1 else 0).
 newton_raphson(Lo_start, Hi_start, Poly) --> (if time(1)==0 then Lo_start elseif time(1)==dt(0) then Hi_start else prev(2)+(prev(1)-prev(2))*last(last(Poly))/(last(last(Poly))-last(Poly))).
-npv(Earnings, Rate) --> sum(Earnings*makearray(Rate^(1-place_in(1)),count(Earnings))).
 true('') --> 1==1.
 false('') --> 1==0.
 pi('') --> 3.1415926535897932384626433832795.
+howmanytrue(BoolList) --> sum(if BoolList then 1 else 0).
+
+% returns the sign of a number, -1 if negative or 1 if positive
+sgn(real) --> choose(real==0,1,int(real/abs(real))).
