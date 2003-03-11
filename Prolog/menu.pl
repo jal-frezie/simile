ca0102ccbbadf719f7b97b011935df0aa58245c4
jmm <jmm>
2003-03-11 16:47:17 +0000

    get_flows Names is now a list of unaltered captions instead of captions with spaces substitied by _

--- Prolog/menu.pl
+++ Prolog/menu.pl
@@ -514,7 +514,7 @@ write_eqn_term(Submodel, Entry, Description, Comment, InFlows, OutFlows) :-
 		Entry = ((CompType:Dest=Eqn) where PPairs))).
 
 get_flows(CompartmentNode, Direction, Names) :-
-	findall(Name,(instance:flows(Direction, CompartmentNode, Arc),caption_for(Arc,Caption),generate_name( prolog, Caption, Name, _ )), Names).
+	findall(Caption,(instance:flows(Direction, CompartmentNode, Arc),caption_for(Arc,Caption)), Names).
 
 get_ppairs([],[]).
 /* Only include a "...where P=V" entry where P is not the default parameter name for V. */
