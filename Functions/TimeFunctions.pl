f9098f6305267b1e584edbeb3f831872a6d618fb
jmm <jmm>
2003-03-31 13:21:08 +0000

    Enclosed names of arrays in brackets as for the equation dialog (as parser now requires)

--- Functions/TimeFunctions.pl
+++ Functions/TimeFunctions.pl
@@ -7,19 +7,19 @@ leapYear( Year ) -->
 
 % dayOfYear
 dayOfYear(Year,Month,Day) -->
-  EndOfPrevMonth  =  [0,31,59,90,120,151,181,212,243,273,304,334],
-  DOY=element(EndOfPrevMonth,Month)+Day, 
+  [EndOfPrevMonth]  =  [0,31,59,90,120,151,181,212,243,273,304,334],
+  DOY=element([EndOfPrevMonth],Month)+Day, 
   choose(leapYear(Year), choose(Month>1,DOY + 1,DOY),DOY).
 
 % Microsoft applications, and hence, most other Windows applications use a floating point number to represent 
 % date and time as number of days from the beginning of 30 December 1989. The integral part of the float is 
 % the date and the fractional part is time of day.
 encodeDateTime(Year,Month,Day,Hour,Minute,Second) -->
-	firstDayOfMonth=[1,32,60,91,121,152,182,213,244,274,305,335],
+	[firstDayOfMonth]=[1,32,60,91,121,152,182,213,244,274,305,335],
 	StartFractionOfDay=(Hour+(Minute+Second/60)/60)/24,
 	years_since_1900=Year-1900,
 	numLeapYrs=int((years_since_1900-1)/4),
-	Date=365*years_since_1900+numLeapYrs+element(firstDayOfMonth,Month)+Day+StartFractionOfDay,
+	Date=365*years_since_1900+numLeapYrs+element([firstDayOfMonth],Month)+Day+StartFractionOfDay,
 	if 4*int(Year/4)==Year and Month>=3 then Date+1 
 	else Date.
 
@@ -31,13 +31,13 @@ dateTime(StartDateTime) -->
 
 % 
 julianDate(Year,Month,Day) -->
-	firstDayOfMonth=[0,31,59,90,120,151,181,212,243,273,304,334],
+	[firstDayOfMonth]=[0,31,59,90,120,151,181,212,243,273,304,334],
 	years_since_0=Year+4712,
 	numLeapYrs=int((years_since_0-1)/4),
         Years=choose(Year>=1583,
 		365*years_since_0+numLeapYrs-10-int((Year-1501)/100)+int((Year-1201)/400),
 		365*years_since_0+numLeapYrs),
-	Date=Years+element(firstDayOfMonth,Month)+Day,		
+	Date=Years+element([firstDayOfMonth],Month)+Day,		
 	if Year==1582 and ((Month==10 and Day>=15) or Month>=11) then Date-10.0
 	elseif 4*int(Year/4)==Year and Month>=3 then Date+1 
 	else Date.
