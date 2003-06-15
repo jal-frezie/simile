dcf5910dceee532e320442fd4181bb8c299bb33f
jaspert <jaspert>
2003-06-15 18:10:37 +0000

    Made RK work for array compartments in tcl

--- Run/hai2mmii.tcl
+++ Run/hai2mmii.tcl
@@ -79,27 +79,20 @@ proc stage_incr {ns_extras step v} {
 	0 {
 	    return $dv
 	} 1 {
-	    set extras(cumulative_value) [expr $dv/6.0]
-#puts "1 $dv $extras(cumulative_value)"
-	    return [set extras(current_offset) [expr $dv/2.0]]
+	    set current_offset [expr $dv/2.0]
+	    set extras [list [expr $dv/6.0] $current_offset]
+	    return $current_offset
 	} 2 {
-	    set extras(cumulative_value) \
-		[expr $extras(cumulative_value)+$dv/3.0]
-#puts "2 $dv $extras(cumulative_value) $extras(current_offset)"
-	    set old_offset $extras(current_offset)
-	    return [expr [set extras(current_offset) [expr $dv/2.0]] \
-			- $old_offset]
+	    set current_offset [expr $dv/2.0]
+	    set old_offset [lindex $extras 1]
+	    set extras [list [expr [lindex $extras 0]+$dv/3.0] $current_offset]
+	    return [expr $current_offset-$old_offset]
 	} 3 {
-	    set extras(cumulative_value) \
-		[expr [set extras(cumulative_value)]+$dv/3.0]
-#puts "3 $dv $extras(cumulative_value) $extras(current_offset)"
-	    set old_offset $extras(current_offset)
-	    return [expr [set extras(current_offset) $dv] - $old_offset]
+	    set old_offset [lindex $extras 1]
+	    set extras [list [expr [lindex $extras 0]+$dv/3.0] $dv]
+	    return [expr $dv-$old_offset]
 	} 4 {
-	    set extras(cumulative_value) \
-		[expr $extras(cumulative_value)+$dv/6.0]
-#puts "4 $dv $extras(cumulative_value) $extras(current_offset)"
-	    return [expr $extras(cumulative_value) - $extras(current_offset)]
+	    return [expr [lindex $extras 0]+$dv/6.0-[lindex $extras 1]]
 	}
     }
 }
