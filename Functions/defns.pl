bbf4e23b36da9ee0d6263e9a185d5e172fd2ac9f
jmm <jmm>
2004-01-31 20:21:36 +0000

    Removed Finns test functions

--- Functions/defns.pl
+++ Functions/defns.pl
@@ -1,7 +1,3 @@
 %function( name_of_function, return type, list_of_parameter_types).
 function(gaussian, real, [real, real, real]).
 function(stop, int, [int]).
-function(grate,real,[real]).
-function(gratea,real,[real]).
-
-
--- Functions/procs.cpp
+++ Functions/procs.cpp
@@ -1,6 +1,3 @@
-//#include <iostream>
-//using namespace std;
-
 double spare;
 BOOLEAN have_spare = 0;
 double gaussian(double random, double mean, double sd) {
@@ -21,21 +18,3 @@ double gaussian(double random, double mean, double sd) {
   return(mean+sd*norm);
 }
     
-double grate(double x)
-{
-  if (x < 5){
-    //cout << 0.01 << endl;
-    return 0.01;
-  }
-  else{
-    //cout << 0.1 << endl;;
-    return 0.1;
-  }  
-}
-
-double gratea(double x)
-{
-  //cout << 0.11 << endl;;
-  return 0.11;
-}
-
