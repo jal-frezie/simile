8679b479ff19b3e68e7042a4d404b0534148b23e
jmm <jmm>
2004-03-17 12:42:06 +0000

    Renamed best(rray)  to posgreatest(A) Added posleast(A)

--- Functions/List handling.pl
+++ Functions/List handling.pl
@@ -15,8 +15,13 @@ first(BoolArr) --> [clear]=makearray(if place_in(1)==1 then 1
 				    elseif element(BoolArr,place_in(1)) then 0
 				    else element(sofar([clear]),place_in(1)-1),
 				     count(BoolArr)),sum([clear])+1.
-best(Incoming) --> [local]=Incoming,[records]=makearray(
+posgreatest(Incoming) --> [local]=Incoming,[records]=makearray(
 			if place_in(1)==1 then 1
 			elseif element([local],place_in(1))>element([local],element(sofar([records]),place_in(1)-1)) then place_in(1)
 			else element(sofar([records]),place_in(1)-1),count([local])),
-element([records],count([local])).
+            element([records],count([local])).
+posleast(Incoming) --> [local]=Incoming,[records]=makearray(
+            if place_in(1)==1 then 1
+            elseif element([local],place_in(1))<element([local],element(sofar([records]),place_in(1)-1)) then place_in(1)
+            else element(sofar([records]),place_in(1)-1),count([local])),
+            element([records],count([local])).
