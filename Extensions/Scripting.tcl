5b6a7d185b40400a0abca73c37508644d97118b8
jmm <jmm>
2004-07-08 10:21:00 +0000

    Changed menu item to "Macros" from "Scripting"

--- Extensions/Scripting.tcl
+++ Extensions/Scripting.tcl
@@ -1,5 +1,7 @@
 package require Itcl
 
+# place all extensions in the ::ModelWindowExtn namespace to allow them
+# to be found easily using itcl::find classes (used in window.tcl MainWindowDraw)
 itcl::class ::ModelWindowExtn::ScriptingExtn {
     inherit ModelWindowExtn
 
@@ -12,9 +14,10 @@ itcl::class ::ModelWindowExtn::ScriptingExtn {
     # hook into each model window this method is called window.tcl MainWindowDraw
     method MergeMenu {} {
         set fm [menu ${winId}top.scripting -tearoff 0]
-        ${winId}top.tools add cascade -label Scripting -underline 0 \
+        ${winId}top.tools add cascade -label Macros -underline 0 \
                 -menu ${winId}top.scripting
         
+        # $this is a built in variable for each object referencing that object
         $fm add command -label "Run ..." -command "$this Run"
         
     }
