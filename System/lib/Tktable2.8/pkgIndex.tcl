ed8373e9cc5230706a972d765896aa2a22b269a6
jaspert <jaspert>
2004-08-09 10:39:17 +0000

    *** empty log message ***

--- System/lib/Tktable2.8/pkgIndex.tcl
+++
@@ -1,6 +0,0 @@
-if {[catch {package require Tcl 8.2}]} return
-switch [info sharedlibextension] {
-    .dll {set imgLib Tktable28.dll}
-    .so {set imgLib libTktable2.8.so}
-}
-package ifneeded Tktable 2.8  [list load [file join $dir $imgLib] Tktable]
