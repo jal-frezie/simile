1963405ba8a85102d5c85a0fd3ec87c3bb5752f7
jaspert <jaspert>
2005-03-10 13:02:52 +0000

    Removed keeps-going-with-no-windows-on-Mac feature until default menu can be sorted Fixed snafu on stub package index

--- Run/window.tcl
+++ Run/window.tcl
@@ -1836,7 +1836,7 @@ proc MenuClose {winId} {
 proc byebye {winId} {
     global window_info tcl_platform
     prolog [list tk_off_window( '$winId' )]
-    if {![string equal Darwin $tcl_platform(os)] && \
+    if { \
 	    ![llength [array names window_info *,is_top_level]]} {
 	prolog tk_kill_everything(_)
     }
