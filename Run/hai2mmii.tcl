09808d6ef76ff279a3b5c26a2428e3f92eb8f603
jaspert <jaspert>
2005-04-07 11:08:42 +0000

    ...not

--- Run/hai2mmii.tcl
+++ Run/hai2mmii.tcl
@@ -78,7 +78,8 @@ proc old_do_model {node what args} {
 
 proc RunningInC {} {
     global model_id myNode
-    return $model_id($myNode)
+    return 0
+    return $model_id($myNode) ;# it is ready
 } 
     
 proc ExplainError {what dest mtime mstep whoopsie} {
