86da719331f5dec4dbd1b24eb5257034f86979f5
jmm <jmm>
2005-06-03 06:57:42 +0000

    Put the following back in # Pop a backslash before chars that would break tcl lists regsub -all {([ ])} $PROLOG_CMD {\\\1} PROLOG_CMD

--- Run/prolog.tcl
+++ Run/prolog.tcl
@@ -130,6 +130,9 @@ set env(GLOBALSZ) [expr $vm_usage/2]
 set env(LOCALSZ) [expr $vm_usage/4]
 set env(TRAILSZ) [expr $vm_usage*3/16]
 
+# Pop a backslash before chars that would break tcl lists
+regsub -all {([ ])} $PROLOG_CMD {\\\1} PROLOG_CMD
+
 set plPipe(err) [file join $env(HOME) .simile log]
 set plPipe(stream) [open |$PROLOG_CMD r+]
 #set plPipe [open "|m:/progra~1/GNU-Prolog/bin/gprolog.exe --init-goal load('../Run/gsimile.wbc') 2> $PROLOG_ERR" r+]
@@ -144,3 +147,4 @@ while {![string match ready $spraf]} {
     }
 }
 KeepLooking
+
