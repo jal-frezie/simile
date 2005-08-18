585662263febebba721e9da1dbe96a814d63570b
jaspert <jaspert>
2005-08-18 13:28:39 +0000

    Fixed 'newton-raphson' so it does not go nan after finding correct value CSV: ----------------------------------------------------------------------

--- Functions/Arithmetic.pl
+++ Functions/Arithmetic.pl
@@ -1,7 +1,7 @@
 %function( name_of_function, return type, list_of_parameter_types).
 function(gaussian, real, [real, real, real]).
 
-newton_raphson(Lo_start, Hi_start, Poly) --> (if time(1)==0 then Lo_start elseif time(1)==dt(0) then Hi_start else prev(2)+(prev(1)-prev(2))*last(last(Poly))/(last(last(Poly))-last(Poly))).
+newton_raphson(Lo_start, Hi_start, Poly) --> (if time(1)==0 then Lo_start elseif time(1)==dt(0) then Hi_start elseif prev(1)==prev(2) then prev(1) else prev(2)+(prev(1)-prev(2))*last(last(Poly))/(last(last(Poly))-last(Poly))).
 
 % returns the sign of a number, -1 if negative or 1 if positive
 sgn(real) --> choose(real==0,1,int(real/abs(real))).
