c39bd828285cb17df3608693756fdc85cf03ded3
jaspert <jaspert>
2006-01-12 15:16:44 +0000

    Fixed 'first' and 'firsttrue' popup messages

--- Functions/Help/macros.tcl
+++ Functions/Help/macros.tcl
@@ -23,7 +23,7 @@ set msgs(pi) {pi(): returns Pi. }
         values, BoolList, that have the value true.}
 set msgs(sgn) {sgn(real): returns the sign of a number, -1 if negative or 1 if positive (or zero).}
 
-set msgs(first) {first(array of booleans): Returns the position of the first element of the array that is true.}
+set msgs(firsttrue) {firsttrue(array of booleans): Returns the position of the first element of the array that is true.}
 set msgs(posgreatest) {posgreatest(array of numbers): Returns the position of the element in the array with the greatest value.}
 set msgs(iterations) {iterations(boolean): Returns the number of times it has been executed since the argument was last true. Typically used with an alarm value as the argument.}
 set msgs(delay) {delay(val, steps): takes a value and an integer count and returns the value as it was that many time steps ago. Works for up to 1000 steps.}
