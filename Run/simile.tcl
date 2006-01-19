db12e1cd2a5622dc9bfa010cc7e3dfa8ce118370
jaspert <jaspert>
2006-01-19 22:44:43 +0000

    Patchlevel raised to p2

--- Run/simile.tcl
+++ Run/simile.tcl
@@ -126,7 +126,7 @@ if {[string equal windows $tcl_platform(platform)]} {
     close $UserStream
 }
 set env(SIMILE_VERSION) 4.5
-set sendvars(simP) {p1}
+set sendvars(simP) {p2}
 
 # KDE launch feedback will fail unless root window is displayed
 # briefly, causing annoying eye candy to persist while program is
