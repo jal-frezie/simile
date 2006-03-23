3c6a0a2cc1eeef79d2d750f18330acde4fa9209f
jaspert <jaspert>
2006-03-23 15:12:57 +0000

    Reduced number of places version number needs to be

--- System/lib/Stubs/pkgIndex.tcl
+++ System/lib/Stubs/pkgIndex.tcl
@@ -11,12 +11,14 @@
 # JAT: this is very old, Windows vs Linux are decided by separate version
 # numbers rather than checking [info sharedlibextension]
 
-set vers $env(SIMILE_VERSION)
-package ifneeded Ame_dll 8.4.$vers.0 \
-    [list load [file join $dir ame_dll84.dll]]
-package ifneeded Ame_dll 8.5.$vers.0 \
-    [list load [file join $dir ame_dll85.dll]]
-package ifneeded Ame_dll 8.4.$vers.1 \
-    [list load [file join $dir libame_dll8.4[info sharedlibextension]]]
-package ifneeded Ame_dll 8.5.$vers.1 \
-    [list load [file join $dir libame_dll8.5[info sharedlibextension]]]
+global env
+if {![catch {set vers $env(SIMILE_VERSION)}]} {
+    package ifneeded Ame_dll 8.4.$vers.0 \
+	[list load [file join $dir ame_dll84.dll]]
+    package ifneeded Ame_dll 8.5.$vers.0 \
+	[list load [file join $dir ame_dll85.dll]]
+    package ifneeded Ame_dll 8.4.$vers.1 \
+	[list load [file join $dir libame_dll8.4[info sharedlibextension]]]
+    package ifneeded Ame_dll 8.5.$vers.1 \
+	[list load [file join $dir libame_dll8.5[info sharedlibextension]]]
+}
