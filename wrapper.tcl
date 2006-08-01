7b47fac00ea6912825e76619da2aca7f5c348a4c
jaspert <jaspert>
2006-08-01 10:54:45 +0000

    Loads 'exec_only' as a tclet

---
+++ wrapper.tcl
@@ -0,0 +1,38 @@
+policy home
+set workingDir Run
+set env(HOME) {}
+set tcl_platform(os) plugin
+set auto_path [list ../System/lib/bwidget1.7]
+
+proc ReadFile {file} {
+    global workingDir
+
+    set fullFile [file join $workingDir $file]
+    return [::browser::getURL $fullFile 3000]
+}
+
+rename source oldsource
+
+proc source {file} {
+    uplevel 1 [ReadFile $file]
+#    if {[catch {uplevel 1 $code} oops]} {
+#	error "$oops sourcing file $file"
+#    }
+}
+
+rename option oldoption
+
+proc option {args} {
+    if {[string equal read [lindex $args 0]]} {
+	# get it from the server and execute it
+    } else {
+	uplevel 1 oldoption $args
+    }
+}
+
+proc cd {newDir} {
+    global workingDir
+    set workingDir [file join $workingDir $newDir]
+}
+
+source exec_only.tcl
