f777c4e47904aa63f1b30bf447f9e6b43f59a376
jaspert <jaspert>
2006-08-11 16:24:40 +0000

    Progress bar added

--- wrapper.tcl
+++ wrapper.tcl
@@ -1,5 +1,6 @@
 policy home
 set workingDir Run
+set fileCount 0
 set env(HOME) {}
 set tcl_platform(os) plugin
 set auto_path [list ../System/lib/bwidget1.7 ../System/lib/calendar]
@@ -16,6 +17,9 @@ proc ReadFile {file} {
 
     set fullFile [file join $workingDir $file]
     ::browser::status "Loading $fullFile"
+    incr fileCount
+    .splash.c coords 2 [list 2 148 [expr {5*$fileCount}] 168]
+    update idletasks
     return [::browser::getURL $fullFile 3000]
     ::browser::status "Done"
 }
@@ -26,15 +30,19 @@ set graph(font) [list helvetica 8]
 frame .splash
 place .splash -x [expr {[winfo width .]/2}] \
     -y [expr {[winfo height .]/2}] -anchor c
-image create photo splash -data [ReadFile ../Images/splash.gif]
 pack [canvas .splash.c -width 400 -height 316 -bd 2] -padx 0 -pady 0
+.splash.c create rect 2 148 398 168 -outline \#999999 -fill \#999999 ;# item 1
+.splash.c create rect 2 148 2 168 -outline \#99cc99 -fill \#99cc99 ;# item 2
+image create photo splash -data [ReadFile ../Images/splash.gif]
 .splash.c create image 200 158 -image splash
 .splash.c create text 245.0 50.0 -font $graph(font) -fill \#99cc99 -anchor w \
     -text "Simulistics Ltd. 2001-2006"
-.splash.c create text 270.0 275.0 -font $graph(font) -fill #660066 -text "Model Web Interface"
+.splash.c create text 270.0 275.0 -font $graph(font) -fill \#660066 -text "Model Web Interface"
 set regInfo "Web Users"
 catch {append regInfo ", Everywhere"}
-.splash.c create text 270.0 295.0 -font $graph(font) -fill #660066 -text "Registered to $regInfo"
+.splash.c create text 270.0 295.0 -font $graph(font) -fill \#660066 -text "Registered to $regInfo"
+.splash.c raise 1
+.splash.c raise 2
 rename source oldsource
 
 proc source {file} {
