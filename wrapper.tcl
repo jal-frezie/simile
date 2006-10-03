c3ec58ea6c9783d91f0def7386f26536390dce61
alastair <alastair>
2006-10-03 14:35:43 +0000

    Changed tabs to spaces

--- wrapper.tcl
+++ wrapper.tcl
@@ -3,22 +3,22 @@ set workingDir /
 
 proc fileSeek {fore aft} {
     if {[string equal relative [file pathtype $aft]]} {
-	foreach bit [file split $aft] {
-	    switch -exact $bit {
-		.. {
-		    set fore [file dirname $fore]
-		    if {[string equal . $fore]} {
-			set fore {}
-		    }
-		} . {
-		} default {
-		    set fore [file join $fore $bit]
-		}
-	    }
-	}
-	return $fore
+        foreach bit [file split $aft] {
+            switch -exact $bit {
+                .. {
+                    set fore [file dirname $fore]
+                    if {[string equal . $fore]} {
+                        set fore {}
+                    }
+                } . {
+                } default {
+                    set fore [file join $fore $bit]
+                }
+            }
+        }
+        return $fore
     } else {
-	return $aft
+        return $aft
     }
 }
 
@@ -35,17 +35,17 @@ proc pwd {} {
 proc checkLikelihood {name} {
     set levels [file split $name]
     if {[string equal tclIndex [lindex $levels end]] || \
-	    [string equal Run [lindex $levels 0]] && \
-	    [string equal pkgIndex.tcl [lindex $levels end]]} {
-	error "Unlikely filename $name"
+            [string equal Run [lindex $levels 0]] && \
+            [string equal pkgIndex.tcl [lindex $levels end]]} {
+        error "Unlikely filename $name"
     }
 }
 
 proc myURL {url} {
     if {[catch {::browser::getURL [MessURL $url] 10000} loss]} {
-	error "Experienced $loss getting $url"
+        error "Experienced $loss getting $url"
     } else {
-	return $loss
+        return $loss
     }
 }
 
@@ -66,7 +66,7 @@ proc ReadFile {file} {
 proc toplevel {win args} {
     eval {frame $win} $args
     place $win -x [expr {[winfo width .]/2}] \
-	-y [expr {[winfo height .]/2}] -anchor c
+        -y [expr {[winfo height .]/2}] -anchor c
 }
 
 proc grab {args} {
@@ -76,41 +76,41 @@ rename wm oldWm
 
 proc wm {act win args} {
     switch -regexp -- $act {
-	withdraw {
-#	    eval {place forget $win} $args
-	} overrideredirect|transient|deiconify {
-	    raise $win
-	} geometry {
+        withdraw {
+#            eval {place forget $win} $args
+        } overrideredirect|transient|deiconify {
+            raise $win
+        } geometry {
 # Once I have ascertained this will work, put something here to position it
-	} default {
-	    return [eval {oldWm $act $win} $args]
-	}
+        } default {
+            return [eval {oldWm $act $win} $args]
+        }
     }
 }
 
 proc TidyURL {name} {
     while {[set special [string first % $name]]!=-1} {
-	set end [expr {$special+2}]
-	if {[scan [string range $name $special $end] "%%%x" num]>0} {
-	    set name [string replace $name $special $end [format %c $num]]
-	} else {
-	    error $name
-	}
+        set end [expr {$special+2}]
+        if {[scan [string range $name $special $end] "%%%x" num]>0} {
+            set name [string replace $name $special $end [format %c $num]]
+        } else {
+            error $name
+        }
     }
     return $name
 }
-		     
+                     
 proc MessURL {name} {
     while {[set spec [string first { } $name]]!=-1} {
-	if {[scan [string range $name $spec $spec] "%c" num]>0} {
-	    set name [string replace $name $spec $spec [format %%%x $num]]
-	} else {
-	    error $name
-	}
+        if {[scan [string range $name $spec $spec] "%c" num]>0} {
+            set name [string replace $name $spec $spec [format %%%x $num]]
+        } else {
+            error $name
+        }
     }
     return $name
 }
-		     
+                     
 rename glob oldGlob
 
 proc glob {args} {
@@ -121,7 +121,7 @@ proc glob {args} {
     set pat $tpt
     
     if {[string equal Run $dir]} {
-	error "unexpectedly globbed $args"
+        error "unexpectedly globbed $args"
     }
 
     ::browser::status "Listing $dir"
@@ -136,15 +136,15 @@ proc glob {args} {
     set results {}
     set file 0
     while {[set file [string first "<a href=\"" $rawData $file]]>-1} {
-	set refStart [expr $file+9]
-	set file [string first "\">" $rawData $refStart]
-	set refEnd [expr $file-1]
-	set file [expr [string first "</a>" $rawData $file]+4]
-	set ref [string range $rawData $refStart $refEnd]
-	if {[string first ? $ref] && [string match $pat $ref] && \
-		[string match relative [file pathtype $ref]]} {
-	    lappend results [TidyURL $ref]
-	}
+        set refStart [expr $file+9]
+        set file [string first "\">" $rawData $refStart]
+        set refEnd [expr $file-1]
+        set file [expr [string first "</a>" $rawData $file]+4]
+        set ref [string range $rawData $refStart $refEnd]
+        if {[string first ? $ref] && [string match $pat $ref] && \
+                [string match relative [file pathtype $ref]]} {
+            lappend results [TidyURL $ref]
+        }
     }
     return $results
 }
@@ -179,7 +179,7 @@ cd /Run
 proc source {file} {
     uplevel 1 [ReadFile $file]
 #    if {[catch {uplevel 1 $code} oops]} {
-#	error "$oops sourcing file $file"
+#        error "$oops sourcing file $file"
 #    }
 }
 
@@ -187,9 +187,9 @@ rename option oldoption
 
 proc option {args} {
     if {[string equal read [lindex $args 0]]} {
-	# get it from the server and execute it
+        # get it from the server and execute it
     } else {
-	uplevel 1 oldoption $args
+        uplevel 1 oldoption $args
     }
 }
 
