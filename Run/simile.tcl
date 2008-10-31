d74112d5ca275ca8a7d06f6668093084ef6efddc
u45169214 <u45169214>
2008-10-31 12:36:01 +0000

    JAT: Patch to a1

--- Run/simile.tcl
+++ Run/simile.tcl
@@ -282,7 +282,7 @@ if {[info exists prolog_in_console]} {
 }
 
 set env(SIMILE_VERSION) 5.4
-set sendvars(simP) {p2}
+set sendvars(simP) {a1}
 
 # KDE launch feedback will fail unless root window is displayed
 # briefly, causing annoying eye candy to persist while program is
