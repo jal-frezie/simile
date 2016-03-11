82ed46dd8094bb4240b7bfd9db0dc86c60aa5ef4
cvs <cvs>
2016-03-11 16:05:06 +0000

    Fixed issue resulting from swi-comp fix earlier today

--- Prolog/language.pl
+++ Prolog/language.pl
@@ -818,7 +818,7 @@ make_section_cond(L, VMPtrs, PassTest) :-
 	all(language, make_new_base_cond,
 	    [unify(L), build(VMPtrs), build(LocaleTests)]),
 	    build_junction(LocaleTests, '||', NewTest),
-	    combine(L, ?, [NewTest, -2:PhaseRef], PassTest)).
+	    combine(L, choose, [NewTest, -2, PhaseRef], PassTest)).
 	    
 	
 make_new_base_cond(L, new_context(Ptr, Phase), LocCond) :-
