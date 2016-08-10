5dd7d1239befd79b4cc017aa01d0b19d2639b132
cvs <cvs>
2016-08-10 11:34:56 +0000

    Initialize time series event properly

--- Prolog/compile.pl
+++ Prolog/compile.pl
@@ -1546,7 +1546,7 @@ get_assignment(instance(Type, Node, Source, DestRef, Unit-DimTypes),
 		UseStep = SmStep),
 	    SourceEqn = Source;
 	(Is_P = 1, apply_minmax(Node, Source, SourceEqn);
-	    Is_P = 3, SourceEqn = prev(0)), % does nothing, works for any dims
+	 Is_P = 3, instance'><'regenerate_makearrays(0, LocalPath, SourceEqn)),
 	    Type = function,
 	    UseList = [on_reset | RefList], 
 	    Made = init(Dest),
