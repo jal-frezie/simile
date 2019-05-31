13f6a4df29cd4abf117ba62b4476feb2324c79f9
jaspert <jaspert>
2019-05-31 16:25:29 +0000

    Version to patch level 14

--- Run/simile.tcl
+++ Run/simile.tcl
@@ -267,7 +267,7 @@ switch $tcl_platform(platform) {
 }
 
 set env(SIMILE_VERSION) 6.9
-set sendvars(simP) {p13}
+set sendvars(simP) {p14}
 
 if {$env(SIMILE_VERSION)>=6.0} {
     set do_events 1 ;# include event symbols
