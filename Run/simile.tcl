7bb8eab070966d1ab66af437d451b3760b8d7000
jaspert <jaspert>
2019-11-29 13:06:14 +0000

    Patch level to p5

--- Run/simile.tcl
+++ Run/simile.tcl
@@ -267,7 +267,7 @@ switch $tcl_platform(platform) {
 }
 
 set env(SIMILE_VERSION) 6.10
-set sendvars(simP) {p4}
+set sendvars(simP) {p5}
 
 if {[package vcompare $env(SIMILE_VERSION) 6.0]>=0} {
     set do_events 1 ;# include event symbols
