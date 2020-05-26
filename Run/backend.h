e1492b7ee1548e3cec0ecfad3b1d37d96efef8b8
jaspert <jaspert>
2020-05-26 14:37:42 +0000

    New dll dispensation now compatible with Windows Admit one-for-all sizing not quite ready

--- Run/backend.h
+++ Run/backend.h
@@ -165,7 +165,7 @@ EXPORT double ame_rand(double, double);
 EXPORT uint64_t seed_rand(int);
 // EXPORT double graphpoint(double, graph_data_type*, int);
 // no need, is declared in dllcalls.h for use by shim)
-EXPORT int compare_instance_status (const int[], const int[],  int);
+int compare_instance_status (const int[], const int[],  int);
 EXPORT void report_events(int, const int[], int, const int[], const double[]);
 EXPORT void handle_model_param_request(void*, void*, int, BOOLEAN, int, int*);
 EXPORT int stat_check(void*);
