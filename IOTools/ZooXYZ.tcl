2578df9ef7badf7ef638e84d74048d662ce1a5fd
jaspert <jaspert>
2023-04-27 12:44:43 +0000

    Went too far with last one

--- IOTools/ZooXYZ.tcl
+++ IOTools/ZooXYZ.tcl
@@ -62,6 +62,7 @@ itcl::class similescript::$newHelperClass {
 	    if {[lindex $inTitle 0] eq ",colours"} {
 		set inTitle [lindex $inTitle 1]
 	    }
+	    set ::helperTable(beingCalled) $this
 	    Display 0 0 0
 	    return
 	}	
@@ -568,6 +569,7 @@ itcl::class similescript::$newHelperClass {
 	    [expr sin($viewVector($winId,elevation))]
 	$winId.c delete -withtag graticule
 	::gen3d1::DrawGrid $winId graticule
+	set ::helperTable(beingCalled) $this
 	Display 0 0 0
     }    
     public method WindowSizeChanged {} {
@@ -577,6 +579,7 @@ itcl::class similescript::$newHelperClass {
 	set viewVector($winId,Y) [winfo height $winId.c]
 	$winId.c delete -withtag graticule
 	::gen3d1::DrawGrid $winId graticule
+	set ::helperTable(beingCalled) $this
 	Display 0 0 0
     }
 
