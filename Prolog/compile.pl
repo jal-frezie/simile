5530c5d9c8420531e53768ff97a20b2d516cec73
jaspert <jaspert>
2023-06-09 08:25:06 +0000

    Reversed ill-advised change to rand_var

--- Prolog/compile.pl
+++ Prolog/compile.pl
@@ -1847,7 +1847,7 @@ get_assignment(instance(Type, Node, Source, DestRef, _Unit-DimTypes),
 	(Is_P = 1, apply_minmax(Node, Source, SourceEqn),
 	  UseStep = SmStep; % was -2 but that allowed variation after reset
 	 % then 0 but changed in v7 to allow default to be e.g., graph(time())
-	 Is_P = 2, \+ Source = use_param_state, SourceEqn = Source, wake,
+	 Is_P = 2, \+ Source = use_param_state, SourceEqn = Source,
 	  UseStep = -1;
 	 Is_P = 3, instance><regenerate_makearrays(0, LocalPath, SourceEqn),
 	  UseStep = 0),
