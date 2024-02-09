36347191681c7813ecca7d1267343120cef39399
jaspert <jaspert>
2024-02-09 15:16:51 +0000

    Add grid to helper toolbar in mre Fix gdal interface for macos

--- Run/mre.tcl
+++ Run/mre.tcl
@@ -56,6 +56,7 @@ namespace eval RunEnv {
    [list \
         [list graph.gif "Create plotter" "::RunEnv::InsertHelperWindow plotter1_dot_25 {}"] \
         [list table.gif "Create table" "::RunEnv::InsertHelperWindow tabular11510 {}"] \
+        [list grid.gif "Create spatial grid" "::RunEnv::InsertHelperWindow grid005 {}"] \
         [list multi.gif "Create layered view" "::RunEnv::InsertHelperWindow Layers20131022 {}"] \
         [list 3d_objects.png "Create 3-D scene" "::RunEnv::InsertHelperWindow Shapes3D20141208 {}"] \
         [list slider.gif "Create input sliders" "::RunEnv::InsertHelperWindow slide139 {}"] \
