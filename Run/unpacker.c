31fc0a9b0e11b7ca806f17f89dc74709dd50cf40
jaspert <jaspert>
2025-03-17 15:56:17 +0000

    Obfuscate my crash generator so it does not cause compiler to generate a warning

--- Run/unpacker.c
+++ Run/unpacker.c
@@ -46,7 +46,7 @@ void crash (Tcl_Interp *interp, const char *cause) {
   // fat chance, we have no Tk in the exec interpreter
   Tcl_VarEval(interp, "ShowMessage {Authorization failure} error {Bad ", cause, " authorization. Simile will now exit.} ok", NULL);
   Tcl_Exit(-1);
-  strcpy((char*)4, "deadbeef"); // that should screw it up nicely
+  strcpy((char*)(secret5-secret), "deadbeef"); // that should screw it up nicely
 }	 
 
 int my_md5(Tcl_Interp *interp, Tcl_Obj* text) {
