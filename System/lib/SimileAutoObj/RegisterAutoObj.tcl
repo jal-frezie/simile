7a74e08cef2813378017ee6587f0aa3505164180
jaspert <jaspert>
2025-07-07 12:14:35 +0000

    Remove obsolete files

--- System/lib/SimileAutoObj/RegisterAutoObj.tcl
+++
@@ -1,6 +0,0 @@
-# to be called from the Simile installation wish84.exe to register the Simile Automation server
-# The could be called by installation program .
-package require tcom
-
-cd ../lib/SimileAutoObj
-::tcom::server register SimileAutoObj.tlb
