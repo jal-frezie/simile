801f1c5bf0ea86977782bb71166dc0dea0e8a4ea
jal_frezie <jasper@simulistics.com>
2026-05-19 15:39:32 +0100

    Steps towards getting internal representations of enumerated type dimensions consistent

--- Prolog/ame_gen.pl
+++ Prolog/ame_gen.pl
@@ -820,11 +820,13 @@ get_node_size(Source, Size) :-
 
 get_node_size(Source, SizeN, Size, Units) :-
 	(Source has_class_refinement multiplication_spec of Multi,
-	   member(count=Dim, Multi);
+	    member(count=Dim, Multi),
+	    ETStyle = bare;
 	  (implicit_function(Source, CompFn); CompFn=Source),
             CompFn has_class_refinement units of Unit,
-	    m_update><analyze_array(Unit, _Base, Dim)), !,
-	get_actual_sizes(Source, Dim, bare, SizeN, Size, Units),
+	    m_update><analyze_array(Unit, _Base, Dim),
+	    ETStyle = quoted), !,
+	get_actual_sizes(Source, Dim, ETStyle, SizeN, Size, Units),
 	(\+ member(var, Size), !;
 	caption_for(Source, Capt),
 	    raise_exception(submodel_size_variable(Capt)));
