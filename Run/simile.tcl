2c12d479c87b786a5b2afadea53cd9c1cc8dab04
jal_frezie <jasper@simulistics.com>
2026-07-09 13:58:15 +0100

    Increment tertiary version

--- Run/simile.tcl
+++ Run/simile.tcl
@@ -301,7 +301,7 @@ switch $tcl_platform(platform) {
 }
 
 set env(SIMILE_VERSION) 7.4
-set sendvars(simP) {.5}
+set sendvars(simP) {.6}
 
 if {[package vcompare $env(SIMILE_VERSION) 6.0]>=0} {
     set do_events 1 ;# include event symbols
